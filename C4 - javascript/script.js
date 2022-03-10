fa0015da40a2d678b38c93965163f5c29504390b
bogdan <bogdanienciu@yahoo.com>
2022-03-10 19:09:46 +0200

    test1

---
+++ C4 - javascript/script.js
@@ -0,0 +1,16 @@
+var firstName = "bogdan";
+var lastName = "ienciu";
+var age = "34";
+
+console.log(firstName);
+
+var actor = {
+    firstName: "bogdan"
+    age: 34
+};
+
+var numbers = [1, 2, 3, 4, 5];
+
+var sum = function (a, b) {
+    return a+b
+}
